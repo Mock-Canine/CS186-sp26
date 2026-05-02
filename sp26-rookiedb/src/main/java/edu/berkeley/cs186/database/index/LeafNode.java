fcec84be54f388be41b365289643bafbdb3c3082
MockCanine <2286596153@qq.com>
2026-05-02 15:25:15 +0800

    finish fromBytes

--- sp26-rookiedb/src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
+++ sp26-rookiedb/src/main/java/edu/berkeley/cs186/database/index/LeafNode.java
@@ -372,12 +372,22 @@ class LeafNode extends BPlusNode {
      */
     public static LeafNode fromBytes(BPlusTreeMetadata metadata, BufferManager bufferManager,
                                      LockContext treeContext, long pageNum) {
-        // TODO(proj2): implement
-        // Note: LeafNode has two constructors. To implement fromBytes be sure to
-        // use the constructor that reuses an existing page instead of fetching a
-        // brand new one.
+        Page page = bufferManager.fetchPage(treeContext, pageNum);
+        Buffer buf = page.getBuffer();
 
-        return null;
+        byte nodeType = buf.get();
+        assert(nodeType == (byte) 1);
+
+        List<DataBox> keys = new ArrayList<>();
+        List<RecordId> rids = new ArrayList<>();
+        long p = buf.getLong();
+        Optional<Long> rightSibling = p == -1L ? Optional.empty() : Optional.of(p);
+        int n = buf.getInt();
+        for (int i = 0; i < n; ++i) {
+            keys.add(DataBox.fromBytes(buf, metadata.getKeySchema()));
+            rids.add(RecordId.fromBytes(buf));
+        }
+        return new LeafNode(metadata, bufferManager, page, keys, rids, rightSibling, treeContext);
     }
 
     // Builtins ////////////////////////////////////////////////////////////////
